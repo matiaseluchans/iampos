2b4984e101470ddc405413e1edf1ccbcde83a8f0
matiaseluchans <35813662+matiaseluchans@users.noreply.github.com>
2025-12-02 08:35:45 -0300

    fix product

--- public/assets/VSpacer-Bm2UjFNg.js
+++
@@ -1,1 +0,0 @@
-import"./VRow-CO8ZPJ7K.js";import{c as r}from"./VAvatar-8GIpnYiE.js";const e=r("v-spacer","div","VSpacer");export{e as V};
---
+++ public/assets/VSpacer-DIkvzS_p.js
@@ -0,0 +1,1 @@
+import"./VRow-SWopTL6l.js";import{c as r}from"./VAvatar-CtGRVD4u.js";const e=r("v-spacer","div","VSpacer");export{e as V};
